e1413f2a40049240ce1cf33b5e9fb4b30bf201cb
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-09 23:17:01 -0800

    initial commit for nfc reader

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform } from 'ionic-angular';
+import { NFC, Ndef } from 'ionic-native';
+
+import { NfcDataComponent } from './../nfc-data/nfc-data.component';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage implements OnInit {
+
+  constructor(public platform: Platform) {}
+
+  ngOnInit() {
+    this.platform.ready().then(() => {
+      this.pushUrl();
+    });
+  }
+
+  pushUrl() {
+    let message1 = Ndef.uriRecord('https://google.com')
+    NFC.share([message1]).then(() => alert('success')).catch(() => alert('error'));
+  }
+}
