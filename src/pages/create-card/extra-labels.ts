8e027c5d09af8903fe9af1caeaa90ec56ae2bf74
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-22 18:36:54 -0800

    feature(create-card) add extra labels to it's own file

---
+++ src/pages/create-card/extra-labels.ts
@@ -0,0 +1,109 @@
+import { Validators } from '@angular/forms';
+
+export const ExtraLabels = {
+  'homeNumber': {
+    label: 'Home Number',
+    formControlName: 'homeNumber',
+    type: 'tel',
+    name: 'phone',
+    validators: [Validators.maxLength(20)]
+  },
+  'mobileNumber': {
+    label: 'Mobile Number',
+    formControlName: 'mobileNumber',
+    type: 'tel',
+    name: 'phone',
+    validators: [Validators.maxLength(20)]
+  },
+  'workNumber': {
+    label: 'Work Number',
+    formControlName: 'workNumber',
+    type: 'tel',
+    name: 'phone',
+    validators: [Validators.maxLength(20)]
+  },
+  'personalNumber': {
+    label: 'Personal Number',
+    formControlName: 'personalNumber',
+    type: 'email',
+    name: '',
+    validators: [Validators.maxLength(20)]
+  },
+  'ext': {
+    label: 'Ext',
+    formControlName: 'ext',
+    type: 'phone',
+    name: 'ext',
+    validators: [Validators.maxLength(20)]
+  },
+  'company': {
+    label: 'Company Name',
+    formControlName: 'company',
+    type: 'text',
+    name: '',
+    validators: [Validators.maxLength(40)]
+  },
+  'address': {
+    label: 'Address',
+    formControlName: 'address',
+    type: 'text',
+    name: 'address',
+    validators: []
+  },
+  'city': {
+    label: 'City',
+    formControlName: 'city',
+    type: 'text',
+    name: 'city',
+    validators: []
+  },
+  'zip': {
+    label: 'Postal / Zip Code',
+    formControlName: 'zip',
+    type: 'text',
+    name: 'zip',
+    validators: []
+  },
+  'country': {
+    label: 'Country',
+    formControlName: 'country',
+    type: 'text',
+    name: 'country',
+    validators: []
+  },
+  'description': {
+    label: 'Description',
+    formControlName: 'description',
+    type: 'text',
+    name: '',
+    validators: []
+  },
+  'faxNumber': {
+    label: 'Fax Number',
+    formControlName: 'faxNumber',
+    type: 'text',
+    name: '',
+    validators: []
+  },
+  'title': {
+    label: 'Position / Title',
+    formControlName: 'title',
+    type: 'text',
+    name: '',
+    validators: []
+  },
+  'tools': {
+    label: 'Tools',
+    formControlName: 'tools',
+    type: 'text',
+    name: '',
+    validators: []
+  },
+  'website': {
+    label: 'Website',
+    formControlName: 'website',
+    type: 'text',
+    name: '',
+    validators: []
+  }
+}
