bef027c148f24e968be0e2e9a1ce97f30b3b4e78
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-23 17:11:33 -0800

    feat(create-card) error checking for required labels
    check for duplicates and max/min length.

--- src/pages/create-card/create-card.ts
+++ src/pages/create-card/create-card.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, ElementRef, QueryList } from '@angular/core';
-import { NavController, NavParams, ActionSheetController, AlertController, ModalController, ViewController } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController, NavParams, ModalController, ViewController } from 'ionic-angular';
 
 import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
@@ -11,6 +11,9 @@ import { ExtraLabels } from './extra-labels';
 
 import { ModalContentPage } from './modal'
 
+import { AccountPage } from '../account/account'
+
+import { ValidateEmail, ValidateDuplicateCardName } from './custom-validators'
 
 @Component({
   selector: 'page-create-card',
@@ -18,41 +21,42 @@ import { ModalContentPage } from './modal'
 })
 
 export class CreateCardPage {
-  @ViewChild('formThis') please: ElementRef;
-
   inputs: Array<any> = [];
 
   alert: any;
 
-  cardType: string = "business";
+  cardType: string;
   label: string = "";
-
-  testRadioOpen: boolean;
-  testRadioResult;
 
   card: Card = {
     cardName: '',
     displayName: '',
-    email: ''
+    email: '',
+    cardType: 'business' // remove when new ones are saved
   };
   cardForm: FormGroup;
   tabBarElement:any;
+
+  cardList: Array<string> = [];
+  originalCardName: string;
+
+  edit: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder,
     public auth: AuthService,
     public navParams: NavParams,
-    public actionSheetCtrl: ActionSheetController,
-    public alertCtrl: AlertController,
     public modalCtrl: ModalController,
-    public viewCtrl: ViewController) {
+    public viewCtrl: ViewController,
+    public navCtrl: NavController) {
+
+    this.cardList = this.auth.cardNameList;
+    this.edit = this.navParams.get('edit');
     this.tabBarElement = document.querySelector('.tabbar');
     this.card.email = navParams.get('email');
     this.card.displayName = navParams.get('displayName');
     this.card.cardName = navParams.get('cardName');
-
-    console.log(viewCtrl.data)
-    console.log(navParams);
+    this.originalCardName = navParams.get('cardName');
   }
 
   ionViewWillEnter() {
@@ -67,6 +71,13 @@ export class CreateCardPage {
   buildForm(): void {
     this.cardForm = this.formBuilder.group({
       // required
+      'cardName': [this.card.cardName, [
+          Validators.required,
+          ValidateDuplicateCardName(this.edit, this.originalCardName, this.cardList),
+          Validators.minLength(1),
+          Validators.maxLength(30)
+        ]
+      ],
       'displayName': [this.card.displayName, [
           Validators.required,
           Validators.minLength(1),
@@ -74,80 +85,30 @@ export class CreateCardPage {
         ]
       ],
       'email': [this.card.email, [
-          this.validateEmail,
+          ValidateEmail,
           Validators.minLength(5),
           Validators.maxLength(255)
         ]
       ],
-      'cardName': [this.card.cardName, [
-          Validators.required,
-          Validators.minLength(1),
-          Validators.maxLength(70)
-        ]
-      ]
+      'cardType': [this.card.cardType, [Validators.required]]
     });
   }
 
   onSubmit({ value, valid }: { value: Card, valid: boolean })  {
-    this.auth.saveCards(value);
-    console.log(value, valid);
-  }
-
-  getCard() {
-    this.auth.getCards();
-  }
-
-  // http://blog.thoughtram.io/angular/2016/03/14/custom-validators-in-angular-2.html
-  validateEmail(c: FormControl) {
-    // http://emailregex.com/
-    let EMAIL_REGEXP = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i
-
-    return EMAIL_REGEXP.test(c.value) ? null : {
-      validateEmail: {
-        valid: false
+    for ( let v in value ) {
+      let trimmedValue = value[v].trim();
+      if (!trimmedValue) {
+        delete value[v];
+      } else {
+        value[v] = trimmedValue;
       }
-    };
-  }
-
-  testButton() {
-    console.log(this.label);
-  }
-
-  createAlert() {
-    this.alert = this.alertCtrl.create();
-    this.alert.setTitle('Labels');
-
-    for (let prop in ExtraLabels) {
-      this.alert.addInput({
-        type: 'checkbox',
-        label: ExtraLabels[prop].label,
-        value: ExtraLabels[prop].formControlName
-      })
     }
-
-    this.alert.addButton('Cancel');
-    this.alert.addButton({
-      text: 'Ok',
-      handler: (data: Array<string>) => {
-        for (let index = 0, len = data.length; index < len; index++) {
-          this.cardForm
-            .addControl(
-              data[index],
-              new FormControl('', ExtraLabels[data[index]].validators));
-          this.inputs.push(ExtraLabels[data[index]]);     
-        }
-
-        this.testRadioOpen = false;
-        this.testRadioResult = data;
-      }
-    });
+    this.auth.saveCards(value);
+    this.navCtrl.setRoot(AccountPage);
   }
 
-  showAlertCheckbox() {
-    this.createAlert();
-    this.alert.present().then(() => {
-      this.testRadioOpen = true;
-    });
+  testForm() {
+    console.log(this.cardForm);
   }
 
   deleteLabel(value, index) {
@@ -168,5 +129,10 @@ export class CreateCardPage {
       }
     })
     modal.present();
+  }
+
+  backButton() {
+    // this.tabBarElement.style.display = 'block';
+    this.navCtrl.setRoot(AccountPage);
   }
 }
