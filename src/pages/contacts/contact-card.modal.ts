9c69dc83cf3f5c36b846fe6f086ade245cfb7303
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-28 21:09:24 -0800

    feat(contact-card) display contact card information

--- src/pages/contacts/contact-card.modal.ts
+++ src/pages/contacts/contact-card.modal.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
-import { Platform, NavParams, ViewController, ModalController } from 'ionic-angular';
+import { Platform, NavParams, ViewController } from 'ionic-angular';
 
-import { ContactCardModal } from './contact-card.modal'
+import { CamelCaseToHeaderPipe } from '../../pipes/camel-case-to-header';
 
 @Component({
   template: `
     <ion-header>
       <ion-toolbar>
         <ion-title>
-          {{displayName}}'s Cards
+          {{displayName}}
         </ion-title>
         <ion-buttons start>
           <button style="font-size: 20px;" showWhen="android,windows" ion-button (click)="dismiss()">
@@ -23,36 +23,33 @@ import { ContactCardModal } from './contact-card.modal'
     </ion-header>
     <ion-content>
       <ion-list>
-        <div *ngFor="let card of cards; let i = index;">
-          <ion-item (click)="openPrompt(i)">
-            <button ion-button block >
-              {{card.displayName}}
-            </button>
+        <div *ngFor="let key of cardKeys; let i = index;">
+          <h4 style="text-align: center;">{{key | camelCaseToHeader }}</h4>
+          <ion-item style="text-align: center;">
+            {{card[key]}}
           </ion-item>
         </div>
       </ion-list>
     </ion-content>
   `
 })
-export class ContentModal {
+export class ContactCardModal {
 
-  cards: any;
+  card: any;
   displayName: string;
+  cardKeys: Array<string> = [];
 
   constructor(
     public platform: Platform,
     public params: NavParams,
-    public viewCtrl: ViewController,
-    public modalCtrl: ModalController) {
+    public viewCtrl: ViewController) {
 
-    this.cards = params.get('cards');
-    this.displayName = params.get('displayName');
-    console.log(this.cards);
-  }
-
-  openPrompt(index) {
-    let modal = this.modalCtrl.create(ContactCardModal, {card: this.cards[index]});
-    modal.present();    
+    this.card = params.get('card');
+    this.displayName = this.card['displayname']
+    let keys = Object.keys(this.card);
+    keys.pop();
+    keys.pop();
+    this.cardKeys = keys;
   }
 
   dismiss() {
