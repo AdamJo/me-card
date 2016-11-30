a9b7e6d411413c34b76804647bf82eabd700ecbf
Adam Johannesmeyer <adamj0101@gmail.com>
2016-11-30 14:11:48 -0800

    chore() remove unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,10 @@ import { MyApp } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HomePage } from '../pages/home/home';
-import { SettingsPage } from '../pages/settings/settings';
 import { NFCPage } from '../pages/nfc/nfc';
-import { AboutPage } from '../pages/about/about';
 import { AccountPage } from '../pages/account/account';
-import { DonateBuyPage } from '../pages/donate-buy/donate-buy';
 import { ContactsPage } from '../pages/contacts/contacts';
-import { LoginOptionsPage } from '../pages/login-options/login-options';
 import { CreateCardPage } from '../pages/create-card/create-card';
-import { ViewEditCardPage } from '../pages/view-edit-card/view-edit-card';
 
 import { ContentModal } from '../modals/contact.modal';
 import { ContactCardModal } from '../modals/contact-card.modal';
@@ -22,7 +17,6 @@ import { CreateCardModal } from '../modals/create-card.modal';
 import { CamelCaseToHeaderPipe } from '../pipes/camel-case-to-header' 
 
 import { TabsPage } from '../pages/tabs/tabs';
-import { TabsLoggedOutPage } from '../pages/tabs-logged-out/tabs-logged-out';
 
 import { AngularFireModule, AuthMethods } from 'angularfire2';
 
@@ -45,17 +39,11 @@ const myFirebaseAuthConfig = {
   declarations: [
     MyApp,
     HomePage,
-    SettingsPage,
     NFCPage,
     AccountPage,
-    AboutPage,
     TabsPage,
     ContactsPage,
-    TabsLoggedOutPage,
-    DonateBuyPage,
-    LoginOptionsPage,
     CreateCardPage,
-    ViewEditCardPage,
     CreateCardModal,
     ContentModal,
     ContactCardModal,
@@ -70,17 +58,11 @@ const myFirebaseAuthConfig = {
   entryComponents: [
     MyApp,
     HomePage,
-    SettingsPage,
     NFCPage,
     AccountPage,
-    AboutPage,
     TabsPage,
     ContactsPage,
-    TabsLoggedOutPage,
-    DonateBuyPage,
-    LoginOptionsPage,
     CreateCardPage,
-    ViewEditCardPage,
     CreateCardModal,
     ContentModal,
     ContactCardModal
